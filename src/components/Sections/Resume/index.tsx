faebc3076debb7bbc769340509713d5821e80578
George Theodorou <theodwrougewrgios@gmail.com>
2023-08-25 12:29:07 +0300

    fix eslint bug

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -1,6 +1,6 @@
 import {FC, memo} from 'react';
 
-import {education, seminars, experience, SectionId, skills} from '../../../data/data';
+import {education, experience, SectionId, seminars, skills} from '../../../data/data';
 import Section from '../../Layout/Section';
 import ResumeSection from './ResumeSection';
 import {SkillGroup} from './Skills';
