4f32f809506b8ebf36900d84f9d6e5bfdcd20cac
George Theodorou <theodwrougewrgios@gmail.com>
2023-08-25 12:46:22 +0300

    Add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
+import {Analytics} from '@vercel/analytics/react';
 import type {AppProps} from 'next/app';
 import {memo} from 'react';
 
@@ -8,6 +9,7 @@ const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 });
