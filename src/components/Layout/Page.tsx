d8511c4c21fce151a8fe254e13936c7207e26f44
George Theodorou <theodwrougewrgios@gmail.com>
2023-08-26 16:39:05 +0300

    change icons

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -13,20 +13,16 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
       <Head>
         <title>{title}</title>
         <meta content={description} name="description" />
-
         {/* several domains list the same content, make sure google knows we mean this one. */}
         <link href={`https://thdgeorge.com${pathname}`} key="canonical" rel="canonical" />
-
         <link href="/favicon.png" rel="icon" sizes="any" />
-        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/favicon.png" rel="icon" type="image/svg+xml" /> {/* /icon.svg */}
+        <link href="/favicon.png" rel="apple-touch-icon" /> {/* /apple-touch-icon.png */}
         <link href="/site.webmanifest" rel="manifest" />
-
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
         <meta content={description} property="og:description" />
         <meta content={`https://thdgeorge.com${pathname}`} property="og:url" />
-
         {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
         <meta content={title} name="twitter:title" />
         <meta content={description} name="twitter:description" />
